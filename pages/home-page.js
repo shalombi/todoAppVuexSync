ae52f773f5325d35dc236a737b75f73ee6238b4b
shalombi <101182935+shalombi@users.noreply.github.com>
2023-01-17 02:47:58 +0200

    Update home-page.js

--- pages/home-page.js
+++ pages/home-page.js
@@ -5,9 +5,8 @@ export default {
         <section class="home">
             <h1> HOME</h1>
             <span>
-            As its name implies, the To-do list on an article's talk page shows the list of improvements suggested for the article. It is created and formatted using the template. The list is maintained by editors, writers, reviewers or readers like you as a way to focus your collaborative efforts. As such, they represent a tentative consensus that helps improve the efficiency of the editing process
+              Welcome to Todos App - cool application for make todo list.
             </span>
-            
         </section>
     
     `,
